0cd0e6fab8a7371d9246f6cf6fbb388c22e9135e
BritishPandaChick <sbartley1080@gmail.com>
2019-05-13 23:05:53 -0400

    Update script file

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 confirm("Are you ready to play?");
 
-var age = prompt("What's your age?");
+var age = prompt("What's your age");
 if(age < 13) {
-  console.log("You are allowed to play, but I take no responsibilty for you.");
+    console.log("You are allowed to play, but I take no responsibility for you.");
 } else {
-  console.log("You can play. Have fun!");
+    console.log("You can play. Have fun!");
 }
 
-console.log("You are at Justin Bieber concert, and you hear this lyric 'Lace my shoes off, star racing.'");
+console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
 console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
 
-var userAnswer = prompt("Do you want to race Biber on strage?");
+var userAnswer = prompt("Do you want to race Bieber on stage?");
 
-if(userAnswer === "yes") {
-  console.log("You and Bieber start racing. It's neck and neck! You win by a shoelace!");
+if (userAnswer === "yes") {
+    console.log("You and Bieber start racing. It's neck and neck! You win by a shoelace!");
 } else {
-  console.log("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'");
+    console.log("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'");
 }
 
-var feedback = prompt("Thank you! We should race at the next concert!");
+var feedback = prompt("Please rate my game from 1-10.");
 
-if (feedback > 8){
-  console.log("Thank you! We should race at the next concert!");
+if (feedback > 8) {
+    console.log("Thank you! We should race at the next concert!");
 } else {
-  console.log("I'll keep practicing coding and racing.");
+    console.log("I'll keep practicing coding and racing.");
 }
